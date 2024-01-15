3647142e99bbc33b6780b8b4e103c4e9222a5847
designersangheon <sangheon8732@gmail.com>
2024-01-16 03:07:24 +0900

    first commit

---
+++ script/scrip.js
@@ -0,0 +1,83 @@
+$(function(){
+    $(".popup-Btna").click(function(){
+        $(".popup-p1").show();
+    });
+    
+    $(".popup-closea").click(function(){
+        $(".popup-p1").hide();
+    });
+    // popup-p1
+
+    $(".popup-Btnb").click(function(){
+        $(".popup-p2").show();
+    });
+    
+    $(".popup-closeb").click(function(){
+        $(".popup-p2").hide();
+    });
+    //  popup-p2
+
+    $(".popup-Btnc").click(function(){
+        $(".popup-p3").show();
+    });
+    
+    $(".popup-closec").click(function(){
+        $(".popup-p3").hide();
+    });
+    //  popup-p3
+
+    $(".popup-Btnd").click(function(){
+        $(".popup-p4").show();
+    });
+    
+    $(".popup-closed").click(function(){
+        $(".popup-p4").hide();
+    });
+    //  popup-p4
+
+    $(".popup-Btne").click(function(){
+        $(".popup-p5").show();
+    });
+    
+    $(".popup-closee").click(function(){
+        $(".popup-p5").hide();
+    });
+    //  popup-p5
+
+    $(".popup-Btnf").click(function(){
+        $(".popup-p6").show();
+    });
+    
+    $(".popup-closef").click(function(){
+        $(".popup-p6").hide();
+    });
+    //  popup-p6
+
+    $(".popup-Btng").click(function(){
+        $(".popup-p7").show();
+    });
+    
+    $(".popup-closeg").click(function(){
+        $(".popup-p7").hide();
+    });
+    //  popup-p7
+
+    $(".popup-Btnh").click(function(){
+        $(".popup-p8").show();
+    });
+    
+    $(".popup-closeh").click(function(){
+        $(".popup-p8").hide();
+    });
+    //  popup-p8
+
+    $(".popup-Btni").click(function(){
+        $(".popup-p9").show();
+    });
+    
+    $(".popup-closei").click(function(){
+        $(".popup-p9").hide();
+    });
+    //  popup-p9
+
+});
